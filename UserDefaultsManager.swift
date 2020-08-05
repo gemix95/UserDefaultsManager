f085bebcfff38d25c09edf371915628dd65cb3ff
gemix <34343587+gemix95@users.noreply.github.com>
2020-08-05 18:38:24 +0200

    Update UserDefaultsManager.swift

--- UserDefaultsManager.swift
+++ UserDefaultsManager.swift
@@ -1,29 +1,28 @@
-import Foundation
-
 class UserDefaultsManager {
     static let shared = UserDefaultsManager()
-
+    
     public enum Key {
         var rawValue: String { return "\(self)" }
         case custom(String)
     }
-
+    
     func getValue<T: Decodable>(_ key: Key, model: T.Type) -> T? {
         guard let data = UserDefaults.standard.data(forKey: key.rawValue) else { return nil }
         return try? JSONDecoder().decode(model, from: data)
     }
-
-    func setValue<T: Codable>(key: Key, data: T) throws {
-        let codableData = Storable(value: data)
-        let data = try JSONEncoder().encode(codableData.value)
+    
+   func setValue<T: Encodable>(key: Key, data: T) throws {
+        let data = try JSONEncoder().encode(data)
         UserDefaults.standard.set(data, forKey: key.rawValue)
     }
-
+    
     func remove(key: Key) {
         UserDefaults.standard.removeObject(forKey: key.rawValue)
     }
-}
-
-public struct Storable<T: Codable>: Codable {
-    let value: T
+    
+    func remove(keys: [Key]) {
+        for key in keys {
+            remove(key: key)
+        }
+    }
 }
